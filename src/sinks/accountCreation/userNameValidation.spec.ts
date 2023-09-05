5f4b99f983c305212536e91d73d845057ac8b828
Gus Narea <gnarea@users.noreply.github.com>
2023-09-05 23:08:54 +0100

    implement user name sanitisation

---
+++ src/sinks/accountCreation/userNameValidation.spec.ts
@@ -0,0 +1,93 @@
+import { sanitiseUserName, RESERVED_WORDS } from './userNameValidation.js';
+
+// eslint-disable-next-line security/detect-unsafe-regex,prefer-named-capture-group,regexp/prefer-named-capture-group
+const GENERATED_NAME_REGEX = /^\w+(-\w+)+$/u;
+
+describe('sanitiseUserName', () => {
+  test('Name should have at least one character', () => {
+    expect(sanitiseUserName('')).toMatch(GENERATED_NAME_REGEX);
+    expect(sanitiseUserName('a')).toBe('a');
+  });
+
+  test('ASCII characters should be allowed', () => {
+    const name = 'alice';
+    expect(sanitiseUserName(name)).toBe(name);
+  });
+
+  test('Unicode characters should be allowed', () => {
+    const name = 'こんにちは';
+    expect(sanitiseUserName(name)).toBe(name);
+  });
+
+  test('Name should be lowercased', () => {
+    expect(sanitiseUserName('ALICE')).toBe('alice');
+    expect(sanitiseUserName('Alice')).toBe('alice');
+    expect(sanitiseUserName('MAÑANA')).toBe('mañana');
+  });
+
+  test('Emojis should be valid', () => {
+    const name = 'hey-👋';
+    expect(sanitiseUserName(name)).toBe(name);
+  });
+
+  describe('Reserved terms', () => {
+    test('Partial match on reserved word should be allowed', () => {
+      const name = 'relay';
+      expect(sanitiseUserName(name)).toBe(name);
+    });
+
+    test.each(RESERVED_WORDS)('Name containing "%s" should be invalid', (word) => {
+      expect(sanitiseUserName(word)).toMatch(GENERATED_NAME_REGEX);
+      expect(sanitiseUserName(word.toUpperCase())).toMatch(GENERATED_NAME_REGEX);
+    });
+
+    test('Name containing obfuscated reserved words should be invalid', () => {
+      expect(sanitiseUserName('gus.narea')).toMatch(GENERATED_NAME_REGEX);
+      expect(sanitiseUserName('gus narea')).toMatch(GENERATED_NAME_REGEX);
+      expect(sanitiseUserName('gus_narea')).toMatch(GENERATED_NAME_REGEX);
+    });
+
+    test('Name containing lookalike reserved words should be invalid', () => {
+      expect(sanitiseUserName('gusnaréa')).toMatch(GENERATED_NAME_REGEX);
+    });
+  });
+
+  describe('Illicit characters', () => {
+    test('Leading whitespace should be removed', () => {
+      expect(sanitiseUserName(' alice')).toBe('alice');
+      expect(sanitiseUserName('\talice')).toBe('alice');
+      expect(sanitiseUserName('\nalice')).toBe('alice');
+      expect(sanitiseUserName('\ralice')).toBe('alice');
+      expect(sanitiseUserName('\r\nalice')).toBe('alice');
+      expect(sanitiseUserName(' \r\nalice')).toBe('alice');
+    });
+
+    test('Trailing whitespace should be removed', () => {
+      expect(sanitiseUserName('alice ')).toBe('alice');
+      expect(sanitiseUserName('alice\t')).toBe('alice');
+      expect(sanitiseUserName('alice\n')).toBe('alice');
+      expect(sanitiseUserName('alice\r')).toBe('alice');
+      expect(sanitiseUserName('alice \r\n')).toBe('alice');
+    });
+
+    test('Whitespace in the middle should be removed', () => {
+      expect(sanitiseUserName('ali ce')).toBe('alice');
+      expect(sanitiseUserName('ali\tce')).toBe('alice');
+      expect(sanitiseUserName('ali\nce')).toBe('alice');
+      expect(sanitiseUserName('ali\rce')).toBe('alice');
+      expect(sanitiseUserName('ali \r\nce')).toBe('alice');
+    });
+
+    test('At signs should be removed', () => {
+      expect(sanitiseUserName('ali@ce')).toBe('alice');
+    });
+  });
+
+  test('Strings longer than 16 characters should be truncated', () => {
+    const validString = 'a'.repeat(16);
+    expect(sanitiseUserName(validString)).toBe(validString);
+
+    expect(sanitiseUserName(`${validString}b`)).toBe(validString);
+    expect(sanitiseUserName(` ${validString} `)).toBe(validString); // Whitespace
+  });
+});
---
+++ src/sinks/accountCreation/userNameValidation.ts
@@ -0,0 +1,40 @@
+import { remove as replaceAsciiLookalike } from 'confusables';
+import { generateUsername } from 'unique-username-generator';
+
+const MAX_USERNAME_LENGTH = 16;
+const ILLICIT_CHARS_REGEX = /[\s@]/gu;
+
+const RESERVED_WORDS = [
+  'admin',
+  'support',
+  'help',
+  'relaycorp',
+  'awala',
+  'letro',
+  'gusnarea',
+  'gustavonarea',
+  'gnarea',
+];
+
+function isUserNameValid(string: string): boolean {
+  const sanitisedString = replaceAsciiLookalike(string).replaceAll(/[^a-z\d]/gu, '');
+  if (sanitisedString.length === 0) {
+    return false;
+  }
+  const hasReservedWords = RESERVED_WORDS.some((word) => sanitisedString.includes(word));
+  return !hasReservedWords;
+}
+
+export { RESERVED_WORDS };
+
+export function sanitiseUserName(name: string): string {
+  const sanitisedName = name
+    .replaceAll(ILLICIT_CHARS_REGEX, '')
+    .slice(0, MAX_USERNAME_LENGTH)
+    .toLowerCase();
+  if (isUserNameValid(sanitisedName)) {
+    return sanitisedName;
+  }
+
+  return generateUsername('-', 0, MAX_USERNAME_LENGTH);
+}
