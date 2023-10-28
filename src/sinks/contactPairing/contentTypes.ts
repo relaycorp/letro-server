5087461314040d0f2197afff4f5d9a6cbc16c4cc
Gus Narea <gnarea@users.noreply.github.com>
2023-10-28 16:02:43 +0100

    Revert renaming of type of contact pairing auth

--- src/sinks/contactPairing/contentTypes.ts
+++ src/sinks/contactPairing/contentTypes.ts
@@ -1,5 +1,5 @@
 export const CONTACT_PAIRING_CONTENT_TYPES = {
   REQUEST: 'application/vnd.relaycorp.letro.contact-pairing.request',
-  AUTHORISATION: 'application/vnd.relaycorp.letro.contact-pairing.authorisation',
+  AUTHORISATION: 'application/vnd.relaycorp.letro.pairing-auth',
   FAILURE: 'application/vnd.relaycorp.letro.contact-pairing.failure',
 };
